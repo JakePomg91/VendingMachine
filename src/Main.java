13fb19e3737916e9867b83ab28931d52373d699f
JAkee <JakePomg94@gmail.com>
2024-05-30 23:50:11 +0300

    Color scheme changed

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product("twix", 60);
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+
+
+    }
+}
